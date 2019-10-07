d6dc640b20d78dd03f9436b31ceef08ef199eef2
Axel Ekwall <axel@minimize.se>
2019-10-07 16:41:08 +0200

    project setup

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Osqledaren',
+  },
+  plugins: [
+    'gatsby-plugin-typescript',
+    // 'gatsby-transformer-json',
+    // 'gatsby-plugin-sharp',
+    // 'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Osqledaren',
+        short_name: 'OL',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#fff',
+        display: 'minimal-ui',
+        // icon: '/', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Index = () => <h1>OL!</h1>;
+
+export default Index;
