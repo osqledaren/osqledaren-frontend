98bc9034084fb0822fbf254a2de9c74bfb18fb14
Axel <git@axelekwall.se>
2019-10-17 15:37:01 +0200

    add readme and some example components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import React, { FunctionComponent } from 'react';
+import { Link } from 'gatsby';
+import H1 from '../atoms/H1';
 
-const Index: FunctionComponent = () => <h1>OL!</h1>;
+const Index: FunctionComponent = () => (
+  <main>
+    <H1 text="Osqledaren" />
+    <Link to="/about">About</Link>
+  </main>
+);
 
 export default Index;
