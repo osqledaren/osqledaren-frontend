bb0d3a2899b2680d233f2191c63f17b65b098a0f
lindacnatt <linda.cnattingius@gmail.com>
2019-12-05 11:49:11 +0100

    add icon in head

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: '#fff',
         theme_color: '#fff',
         display: 'minimal-ui',
-        // icon: '/', // This path is relative to the root of the site.
+        icon: './src/images/logo-small.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
