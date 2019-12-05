66ff8490dc4c832423ebeffb837fbc188b585eac
Axel Ekwall <git@axelekwall.se>
2019-12-05 11:11:16 +0100

    add B

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import React, { FunctionComponent } from 'react';
 import H from '../atoms/H';
 import Layout from '../organisms/Layout';
+import P from '../atoms/P';
+import B from '../atoms/B';
 
 const Index: FunctionComponent = () => (
   <main>
     <Layout>
       <H>Main content</H>
+      <P>
+        Hej <B>bold</B>
+      </P>
     </Layout>
   </main>
 );
