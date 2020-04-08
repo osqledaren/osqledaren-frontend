c994b864d6e8e1677c1bd42a064a3fed77687166
JOhohannes Loor <johannes.loor@gmail.com>
2020-04-08 13:04:33 +0200

    Moved arrow on smaller screens

--- src/molecules/ArticleCard.tsx
+++ src/molecules/ArticleCard.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState } from 'react';
 import { graphql, useStaticQuery, Link } from 'gatsby';
 import MediaCard from './MediaCard';
 import Img from 'gatsby-image';
+import arrowImg from '../images/down.png';
 import styled from '../styles/styled';
 import H from '../atoms/H';
 import P from '../atoms/P';
@@ -21,17 +22,6 @@ const ArticleCard: FunctionComponent<Props> = ({
   to = '/',
   expand = true,
 }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      arrow: file(relativePath: { eq: "down.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1000) {
-            ...GatsbyImageSharpFluid_noBase64
-          }
-        }
-      }
-    }
-  `);
   const [isExpanded, setExpand] = useState(expand);
 
   function checkExpand() {
@@ -40,34 +30,45 @@ const ArticleCard: FunctionComponent<Props> = ({
 
   return (
     <MediaCard category={category}>
-      <LinkWrapper to={to}>
-        <ArticleContent reverse={reverse} expand={isExpanded}>
-          {mainImage && (
-            <Image expand={isExpanded}>
-              <Img fluid={mainImage.asset.fluid}></Img>
-            </Image>
-          )}
-          <Text expand={isExpanded} reverse={reverse}>
-            <Title variant="4">{title ? title : 'Titel saknas'}</Title>
-            <Date size="11" expand={isExpanded}>
-              {publishDate ? publishDate : 'Datum saknas'}
-            </Date>
-            <Description expand={isExpanded}>
-              {ingress
-                ? ingress
-                : 'Beskrivning saknas Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-            </Description>
-          </Text>
-        </ArticleContent>
-      </LinkWrapper>
+      <ArticleWrapper>
+        <LinkWrapper to={to}>
+          <ArticleContent reverse={reverse} expand={isExpanded}>
+            {mainImage && (
+              <Image expand={isExpanded}>
+                <Img fluid={mainImage.asset.fluid}></Img>
+              </Image>
+            )}
+            <Text expand={isExpanded} reverse={reverse}>
+              <Title variant="4">{title ? title : 'Titel saknas'}</Title>
+              <Date size="11" expand={isExpanded}>
+                {publishDate ? publishDate : 'Datum saknas'}
+              </Date>
+              <Description expand={isExpanded}>
+                {ingress
+                  ? ingress
+                  : 'Beskrivning saknas Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+              </Description>
+            </Text>
+          </ArticleContent>
+        </LinkWrapper>
 
-      <Arrow expand={isExpanded} onClick={checkExpand}>
-        <Img fluid={data.arrow.childImageSharp.fluid}></Img>
-      </Arrow>
+        <ArrowWrapper expand={isExpanded} onClick={checkExpand}>
+          <Arrow expand={isExpanded} src={arrowImg}></Arrow>
+        </ArrowWrapper>
+      </ArticleWrapper>
     </MediaCard>
   );
 };
 
+const ArticleWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  @media (max-width: ${props => {
+      return props.theme.breakpoints.sm + 'px';
+    }}) {
+    flex-direction: column;
+  }
+`;
 const LinkWrapper = styled(Link)`
   width: 95%;
   text-decoration: none;
@@ -135,7 +136,7 @@ const Title = styled(H)`
   @media (max-width: ${props => {
       return props.theme.breakpoints.md + 'px';
     }}) {
-    font-size: 20px;
+    font-size: 18px;
     padding-top: 0rem;
   }
 `;
@@ -185,19 +186,26 @@ const Description = styled(P)<{ expand: boolean }>`
   }
 `;
 
-const Arrow = styled.div<{ expand: boolean }>`
-  height: 100%;
-  width: 5%;
-  margin-right: 2%;
+const ArrowWrapper = styled.div<{ expand: boolean }>`
+  display: flex;
+  height: 100% - 1rem;
+  padding-top: 1rem;
+  padding-right: 0.5rem;
   @media (max-width: ${props => {
       return props.theme.breakpoints.sm + 'px';
     }}) {
-    width: 8%;
+    width: 100%;
+    justify-content: center;
+    padding: 0 0 0.4rem 0;
   }
   &:hover {
     cursor: pointer;
   }
-  padding-top: 1rem;
+`;
+
+const Arrow = styled.img<{ expand: boolean }>`
+  width: 25px;
+  height: 25px;
   transform: ${props => {
     return props.expand ? 'rotate(180deg)' : '';
   }};
