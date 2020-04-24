5caf64bb235dbdf1547ab74a6797a804b8447762
JOhohannes Loor <johannes.loor@gmail.com>
2020-04-24 15:15:15 +0200

    Added temporary podcast info

--- src/pages/podcasts.tsx
+++ src/pages/podcasts.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import Layout from '../organisms/Layout';
 import { Article } from '../utils/types';
 import ContentWrapper from '../molecules/ContentWrapper';
+import H from '../atoms/H';
+import styled from '../styles/styled';
 
 interface Props {
   pageContext: {
@@ -18,8 +20,20 @@ interface Props {
 
 const Podcasts: FC<Props> = () => (
   <Layout>
-    <ContentWrapper>Podcasts here</ContentWrapper>
+    <ContentWrapper>
+      <PodcastWrapper>
+        <H>Här kommer du snart att kunna lyssna på podcasts från OL!</H>
+        <H>(Podcasts from OL coming soon)</H>
+      </PodcastWrapper>
+    </ContentWrapper>
   </Layout>
 );
 
 export default Podcasts;
+
+const PodcastWrapper = styled.div`
+  padding: 20px;
+  margin: auto;
+  background-color: white;
+  border-radius: 20px;
+`;
