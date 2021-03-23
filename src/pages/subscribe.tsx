247ea31e231648a40b51526a94aa32957a01e295
Moa <moa.winberg.1@gmail.com>
2021-03-23 13:14:42 +0100

    change google form link

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -7,7 +7,7 @@ const Subscribe: FunctionComponent = () => {
   return (
     <Layout>
       <InfoWrapper>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdvwSrOgJ9GiJ0whnDvjKNvmfiffqGAC2z3PjxVeLoFKbZf4g/viewform?embedded=true" width="640" height="1130" frameBorder="0" marginHeight={0} marginWidth={0}>Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd9nWTHHGgAyT03t-WTH9XEgA8J-prcYSsBv_PTZNSexOkSeg/viewform?embedded=true" width="640" height="1428" frameBorder="0" marginHeight={0} marginWidth={0}>Loading…</iframe>
       </InfoWrapper>
     </Layout>
   );
